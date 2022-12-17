b48fab826098c1bed9b679c0283a4149e658d459
suman3780 <66299997+suman3780@users.noreply.github.com>
2022-12-17 12:21:05 +0530

    Update App.Js
    rollback

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from "./logo.svg";
-import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function App({ signOut }) {
+function App() {
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
